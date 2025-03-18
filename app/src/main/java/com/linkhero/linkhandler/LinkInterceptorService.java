c4b95a8f3c631164093fc3151c55a40d4ae11eb5
DeepX93 <deepxtimacy@gmail.com>
2025-03-18 17:44:59 +0530

    Second Set Up

--- app/src/main/java/com/linkhero/linkhandler/LinkInterceptorService.java
+++ app/src/main/java/com/linkhero/linkhandler/LinkInterceptorService.java
@@ -2,17 +2,20 @@ package com.linkhero.linkhandler;
 
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.IBinder;
+import android.util.Log;
 
 public class LinkInterceptorService extends Service {
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // Placeholder for link interception logic
-        return START_STICKY;
-    }
-}
+        if (intent != null && Intent.ACTION_VIEW.equals(intent.getAction())) {
+            Uri data = intent.getData();
+            if (data != null) {
+                String interceptedLink = data.toString();
+                Log.d("LinkInterceptor", "Intercepted Link: " + interceptedLink);
+
+                // Send link to MacroDroid
+    
+
